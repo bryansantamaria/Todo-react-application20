291def7a018f172114e7378190292ad1a6595eff
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-24 20:31:37 +0200

    added a simple error handler on creating a user, if email already exsists it does not insert into db

--- root-react/server/models/userModel.js
+++ root-react/server/models/userModel.js
@@ -2,15 +2,18 @@ const { userCollection } = require("../database/dataBase");
 const bcrypt = require("bcryptjs");
 
 const createUser = async (firstName, lastName, password, email) => {
-  const hash = bcrypt.hashSync(password, 10);
-  console.log(hash);
-  const doc = await userCollection.insert({
-    firstName,
-    lastName,
-    password: hash,
-    email,
-  });
-  return doc;
+  const uniqueEmail = await userCollection.find({ email: email });
+  if (!uniqueEmail) {
+    const hash = bcrypt.hashSync(password, 10);
+    const doc = await userCollection.insert({
+      firstName,
+      lastName,
+      password: hash,
+      email,
+    });
+    return doc;
+  }
+  return console.log("EMAIL already registered!");
 };
 
 module.exports = { createUser };
