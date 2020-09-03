91853ef944d970258cceb20633003278504e694a
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-03 22:54:37 +0200

    Setup test and Dev environments in database and .env file

--- root-react/server/database/dataBase.js
+++ root-react/server/database/dataBase.js
@@ -1,12 +1,44 @@
-const Datastore = require("nedb-promises");
-const toDoCollection = new Datastore({
-  filename: "./database/ToDos.db",
-  autoload: true,
-});
+const Datastore = require('nedb-promises');
+require('dotenv').config();
 
-const userCollection = new Datastore({
-  filename: "./database/Users.db",
-  autoload: true,
-});
+let toDoCollection, itemCollection, userCollection;
+switch (process.env.ENVIRONMENT) {
+	case 'development':
+		toDoCollection = new Datastore({
+			filename: './database/ToDos.db',
+			autoload: true,
+		});
 
-module.exports = { toDoCollection, userCollection };
+		userCollection = new Datastore({
+			filename: './database/Users.db',
+			autoload: true,
+		});
+
+		itemCollection = new Datastore({
+			filename: './database/Items.db',
+			autoload: true,
+		});
+		break;
+
+	case 'test':
+		toDoCollection = new Datastore({
+			filename: './database/test_ToDos.db',
+			autoload: true,
+		});
+
+		userCollection = new Datastore({
+			filename: './database/test_Users.db',
+			autoload: true,
+		});
+
+		itemCollection = new Datastore({
+			filename: './database/test_Items.db',
+			autoload: true,
+		});
+
+	// toDoCollection.remove({});
+	// itemCollection.remove({});
+	// userCollection.remove({});
+}
+
+module.exports = { toDoCollection, itemCollection, userCollection };
