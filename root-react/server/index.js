316178d4f5f13842548c61e5da58cf506f6fe438
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-03 22:56:13 +0200

    exported app, setup a start.js file for chai-http tests

--- root-react/server/index.js
+++ root-react/server/index.js
@@ -1,26 +1,23 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const usersRoute = require("./routes/userRoute");
-const toDosRoute = require("./routes/toDoRoute.js");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const usersRoute = require('./routes/userRoute');
+const toDosRoute = require('./routes/toDoRoute.js');
+const cors = require('cors');
 
 const app = express();
-const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", usersRoute);
-app.use("/todo", toDosRoute);
+app.use('/', usersRoute);
+app.use('/todo', toDosRoute);
 
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening to port: ${PORT}`);
-});
+module.exports = app;
