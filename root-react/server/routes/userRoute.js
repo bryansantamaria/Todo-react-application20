8efcea81af645c5c16427c6cec5721bd1554b2cc
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-04 22:12:51 +0200

    Item Model added, CRUD models for inserting, updating, deleting and getting form item collection

--- root-react/server/routes/userRoute.js
+++ root-react/server/routes/userRoute.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { create, login, getUser } = require("../controllers/userController");
-const { authenticate } = require("../middlewares/auth");
+const { create, login, getUser } = require('../controllers/userController');
+const { authenticate } = require('../middlewares/auth');
 
-router.post("/create", authenticate, create);
-router.post("/auth", login);
-router.get("/users", authenticate, getUser);
+router.post('/create', authenticate, create);
+router.post('/auth', login);
+router.get('/', authenticate, getUser);
 module.exports = router;
