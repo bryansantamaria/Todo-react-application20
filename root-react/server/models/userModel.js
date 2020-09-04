4d988883314235463b9b6c6447af6ab6cd418d20
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-04 22:14:48 +0200

    updated userController

--- root-react/server/models/userModel.js
+++ root-react/server/models/userModel.js
@@ -1,36 +1,46 @@
-const { createUser, loginUser, checkAuthorization } = require('../models/userModel');
+const { userCollection } = require('../database/dataBase');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const create = async (req, res) => {
-	const { firstName, lastName, email, password } = req.body;
-	try {
-		if (await checkAuthorization(req.user.role)) {
-			console.log('Authorized!!');
-			const doc = await createUser(firstName, lastName, email, password);
-			return res.status(200).json(doc);
+const createUser = async (firstName, lastName, email, password, role) => {
+	const doc = await userCollection.findOne({ email: email });
+	if (role === 'admin') {
+		if (!doc) {
+			const hash = bcrypt.hashSync(password, 10);
+			const doc = await userCollection.insert({
+				firstName,
+				lastName,
+				password: hash,
+				email,
+				role: 'user',
+			});
+			return doc;
 		}
-	} catch (error) {
-		return res.status(401).json(error);
+		return console.log('EMAIL already registered!');
 	}
 };
 
-const login = async (req, res) => {
-	const { email, password } = req.body;
-	try {
-		const token = await loginUser(email, password);
-		return res.status(200).json(token);
-	} catch (error) {
-		return res.status(401).json(error);
-	}
+const loginUser = async (email, password) => {
+	const doc = await userCollection.findOne({ email: email });
+	if (!doc) res.status(403).json('Email not found');
+
+	const success = await bcrypt.compareSync(password, doc.password);
+	if (!success) res.status(403).json('Wrong password');
+
+	const token = await jwt.sign(
+		{ email: doc.email, userId: doc._id, role: doc.role, name: doc.firstName },
+		process.env.SECRET,
+		{
+			expiresIn: '1h',
+		}
+	);
+	console.log(token);
+	return token;
 };
 
-const getUser = async (req, res) => {
-	try {
-		console.log(req.user);
-		const { name, role } = req.user;
-		return res.status(200).json({ name, role });
-	} catch (error) {
-		return res.status(401).json(error);
-	}
+const verifyToken = async (token, secret) => {
+	const validToken = await jwt.verify(token, secret);
+	return validToken;
 };
 
-module.exports = { create, login, getUser };
+module.exports = { createUser, loginUser, verifyToken };
