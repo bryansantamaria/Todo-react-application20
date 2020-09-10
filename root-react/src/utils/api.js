df62041e1481d208203fa4027500710afc438b04
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-10 17:19:09 +0200

    Add CRUD events to API

--- root-react/src/utils/api.js
+++ root-react/src/utils/api.js
@@ -1,159 +1,210 @@
-import axios from "axios";
-
-export const getTodos = async (url, token) => {
-  console.log(token);
-  const data = await axios
-    .get(url, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      return res;
-    });
-  return data;
-};
+import axios from 'axios';
 
 export const getUser = async (url, token) => {
-  const data = await axios
-    .get(url, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      return res;
-    });
-  return data;
+	const data = await axios
+		.get(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
 };
 
+export const getToDo = async (url, token) => {
+	const data = await axios
+		.get(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
+};
 export const postToDo = async (url, title, token) => {
-  console.log(token);
-  const data = await axios
-    .post(
-      url,
-      {
-        title,
-        done: false,
-      },
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-    .then((res) => {
-      console.log(res);
-      return res;
-    });
-  return data;
+	const data = await axios
+		.post(
+			url,
+			{
+				title,
+			},
+			{
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		)
+		.then((res) => {
+			console.log(res);
+			return res;
+		});
+	return data;
+};
+
+export const getToDoWithItems = async (url, token) => {
+	const data = await axios
+		.get(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
 };
 
 export const delToDo = async (url, token) => {
-  const data = await axios
-    .delete(url, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      return res;
-    });
-  return data;
+	const data = await axios
+		.delete(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
 };
 
-export const patchToDo = async (url, title, token) => {
-  const data = await axios
-    .patch(
-      url,
-      {
-        title,
-        done: false,
-      },
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-    .then((res) => {
-      return res;
-    });
-  console.log(data);
-  return data;
+export const postItem = async (url, title, toDoId, token) => {
+	const data = await axios
+		.post(
+			url,
+			{
+				title,
+				done: false,
+				toDoId,
+			},
+			{
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		)
+		.then((res) => {
+			console.log(res);
+			return res;
+		});
+	return data;
+};
+
+export const getItems = async (url, token) => {
+	const data = await axios
+		.get(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
+};
+
+export const delItem = async (url, token) => {
+	const data = await axios
+		.delete(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
+};
+
+export const patchItem = async (url, title, token) => {
+	const data = await axios
+		.patch(
+			url,
+			{
+				title,
+				done: false,
+			},
+			{
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		)
+		.then((res) => {
+			return res;
+		});
+	console.log(data);
+	return data;
 };
 
 export const updateCompleted = async (url, title, done, token) => {
-  const data = await axios
-    .patch(
-      url,
-      {
-        title,
-        done,
-      },
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-    .then((res) => {
-      return res;
-    });
-  console.log(data);
-  return data;
+	const data = await axios
+		.patch(
+			url,
+			{
+				title,
+				done,
+			},
+			{
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		)
+		.then((res) => {
+			return res;
+		});
+	console.log(data);
+	return data;
 };
 
 export const getOrderBy = async (url, token) => {
-  const data = await await axios
-    .get(url, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      return res;
-    });
-  return data;
+	const data = await await axios
+		.get(url, {
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
 };
 
-export const postAccount = async (
-  firstName,
-  lastName,
-  email,
-  password,
-  token
-) => {
-  const data = await axios
-    .post(
-      "http://localhost:8080/create",
-      {
-        firstName,
-        lastName,
-        email,
-        password,
-      },
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-    .then((res) => {
-      console.log(res);
-      return res;
-    });
-  return data;
+export const postAccount = async (firstName, lastName, email, password, token) => {
+	const data = await axios
+		.post(
+			'http://localhost:8080/users/create',
+			{
+				firstName,
+				lastName,
+				email,
+				password,
+			},
+			{
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		)
+		.then((res) => {
+			console.log(res);
+			return res;
+		});
+	return data;
 };
 
 export const postLogin = async (email, password) => {
-  const data = await axios
-    .post("http://localhost:8080/auth", {
-      email,
-      password,
-    })
-    .then((res) => {
-      return res;
-    });
-  return data;
+	const data = await axios
+		.post('http://localhost:8080/users/auth', {
+			email,
+			password,
+		})
+		.then((res) => {
+			return res;
+		});
+	return data;
 };
