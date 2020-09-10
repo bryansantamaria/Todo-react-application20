2956e07a79b7b73210fb6540c89008ad87df096a
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-10 17:14:55 +0200

    moved token in auth to try catch

--- root-react/server/middlewares/auth.js
+++ root-react/server/middlewares/auth.js
@@ -1,17 +1,17 @@
-const { verifyToken } = require("../models/userModel");
+const { verifyToken } = require('../models/userModel');
 
 const authenticate = async (req, res, next) => {
-  if (!req.headers.authorization) return res.sendStatus(403);
-  const token = req.headers.authorization.replace("Bearer ", "");
+	if (!req.headers.authorization) return res.sendStatus(403);
 
-  try {
-    const payload = await verifyToken(token, process.env.SECRET);
-    req.user = payload;
-    console.log(req.user);
-    next();
-  } catch (error) {
-    return res.sendStatus(401).json(error);
-  }
+	try {
+		const token = req.headers.authorization.replace('Bearer ', '');
+		const payload = await verifyToken(token, process.env.SECRET);
+		req.user = payload;
+		console.log(req.user);
+		next();
+	} catch (error) {
+		return res.sendStatus(401).json(error);
+	}
 };
 
 module.exports = { authenticate };
